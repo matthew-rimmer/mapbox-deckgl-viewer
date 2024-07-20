384a953a6254909fa7c702b20f0c3b6c65b58c57
joshnice <joshliamnice@gmail.com>
2024-07-20 16:54:47 +0100

    Remove aws stuff

--- aws/bin/aws.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { MapDeckGlb } from '../lib/aws-stack';
-
-const app = new cdk.App();
-new MapDeckGlb(app, 'map-deck-glb');
